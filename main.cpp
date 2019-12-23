be0e5f8cac8c9a4036a0095b046078b57854602c
Chadwick Boulay <chadwick.boulay@gmail.com>
2019-12-23 09:44:54 -0500

    Fixed missing include and updated link.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 /******************************************************************************************
 Application to allow the viewing and editing of XDF files. Currently, only text chunks 
 can be viewed and edited. The specifications for xdf files are available at 
-https://code.google.com/p/xdf/wiki/Specifications.
+https://github.com/sccn/xdf/wiki/Specifications.
 
 Written by Matthew Grivich, SCCN/UCSD, May 2014.
 
 *******************************************************************************************/
-
+#include "mainwindow.h"
 #include <QApplication>
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
